8d7b1b5627b0292c8956c2275297a2366044ac4c
Ara <ara@mautic>
2018-04-24 18:52:46 +0200

    Added Rsync command

--- pgins.sh
+++ pgins.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rsync -azP ara@192.95.0.118:/opt/offers/*.json /home/ara/offers
+
 python3 pg_insdata.py -t tesco
 python3 pg_insdata.py -t sainsburys
 python3 pg_insdata.py -t morrison
