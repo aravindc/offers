829796a06bc092308b0fe3545460b813d389dc51
Ara <ara@mautic>
2018-07-01 00:53:47 +0200

    Updated pgins.sh to use orm code

--- pgins.sh
+++ pgins.sh
@@ -2,8 +2,14 @@
 
 rsync -azP ara@192.95.0.118:/opt/offers/*.json /home/ara/offers
 
-python3 pg_insdata.py -t tesco
-python3 pg_insdata.py -t sainsburys
-python3 pg_insdata.py -t morrison
-python3 pg_insdata.py -t ocado
-python3 pg_insdata.py -t asda
+#python3 pg_insdata.py -t tesco
+#python3 pg_insdata.py -t sainsburys
+#python3 pg_insdata.py -t morrison
+#python3 pg_insdata.py -t ocado
+#python3 pg_insdata.py -t asda
+
+python3 pg_ormins.py -t single -r tesco
+python3 pg_ormins.py -t single -r sainsburys
+python3 pg_ormins.py -t single -r morrison
+python3 pg_ormins.py -t single -r ocado
+python3 pg_ormins.py -t single -r asda
