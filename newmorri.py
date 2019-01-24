391739d27e43f0701bf7ff69e688fb4c4535c7e6
ara <aravindc@sivapriya.org>
2019-01-24 14:27:59 +0000

    Fix with additional api

--- newmorri.py
+++ newmorri.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import requests
 import io
+import time
 
 # Initialize logger
 logging.basicConfig(level=logging.INFO)
@@ -17,60 +18,122 @@ logger.addHandler(hdlr)
 
 FILE_NAME = '/opt/offers/MORRI_' + datetime.now().strftime("%Y%m%d") + '.json'
 
-morris_urls = {
-    "base_url": "https://www.ocado.com/webshop/api/v1/browse?tags=19998",
-    "img_base": "https://www.ocado.com/productImages/{3}/{0}_0_{1}x{2}.jpg",
-    "prod_base": "https://www.ocado.com/webshop/product/{0}/{1}",
-    "promo_base": "https://www.ocado.com/webshop/promotion/{0}/{1}"
+occado_urls = {
+    "base_url": "https://groceries.morrisons.com/webshop/api/v1/browse?tags=19998",
+    "img_base": "https://groceries.morrisons.com/productImages/{3}/{0}_0_{1}x{2}.jpg",
+    "prod_base": "https://groceries.morrisons.com/webshop/product/{0}/{1}",
+    "promo_base": "https://groceries.morrisons.com/webshop/promotion/{0}/{1}",
+    "sku_base": "https://groceries.morrisons.com/webshop/api/v1/products?skus={0}"
 }
 
 
-def getOffers():
-    response = requests.get(morris_urls['base_url'])
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+def getSkuUrls(skus):
+    sku_urls = []
+    for sku in chunker(skus, 20):
+        sku_urls.append(occado_urls['sku_base'].format(
+            ",".join(str(x) for x in sku)))
+    return sku_urls
+
+
+def getOffers(skuUrls):
+
+    for skuUrl in skuUrls:
+        response = requests.get(skuUrl)
+        json_obj = json.loads(response.text)
+        full_data = []
+        missing_data = []
+        for obj in json_obj:
+            json_data = {}
+            logger.info(obj)
+
+            if 'name' not in obj and 'product' not in obj:
+                missing_data.append(obj)
+                continue
+
+            if 'product' in obj:
+                loc_sku = obj['product']['sku']
+                loc_name = obj['product']['name']
+                loc_price = obj['product']['price']['current']
+                if 'unit' in obj['product']['price']:
+                    loc_unit_price = obj['product']['price']['unit']['price']
+                    loc_unit = obj['product']['price']['unit']['per']
+                else:
+                    loc_unit_price = "N/A"
+                    loc_unit = "N/A"
+                loc_category = obj['product']['mainCategory']
+                repl_chars = '£ ,!"^&*()%'
+                loc_offtext = obj['product']['offer']['text'].lower()
+                for char in repl_chars:
+                    loc_offtext = loc_offtext.replace(char, '-')
+                loc_offtext = loc_offtext.replace('--', '-')
+                loc_offid = obj['product']['offer']['id']
+            else:
+                loc_sku = obj['sku']
+                loc_name = obj['name']
+                loc_price = obj['price']['current']
+                if 'unit' in obj['price']:
+                    loc_unit_price = obj['price']['unit']['price']
+                    loc_unit = obj['price']['unit']['per']
+                else:
+                    loc_unit_price = "N/A"
+                    loc_unit = "N/A"
+                loc_category = obj['mainCategory']
+                repl_chars = '£ ,!"^&*()%'
+                loc_offtext = obj['offer']['text'].lower()
+                for char in repl_chars:
+                    loc_offtext = loc_offtext.replace(char, '-')
+                loc_offtext = loc_offtext.replace('--', '-')
+                loc_offid = obj['offer']['id']
+
+            logger.info(loc_sku)
+            json_data['prod_sku'] = loc_sku
+            json_data['prod_name'] = loc_name
+            json_data['prod_url'] = occado_urls['prod_base'].format(
+                loc_name.replace(' ', '-'), loc_sku)
+            json_data['promo_url'] = occado_urls['promo_base'].format(
+                loc_offtext, loc_offid)
+            json_data['prod_150_img'] = occado_urls['img_base'].format(
+                loc_sku, 150, 150, loc_sku[:3])
+            json_data['prod_640_img'] = occado_urls['img_base'].format(
+                loc_sku, 640, 640, loc_sku[:3])
+            json_data['prod_price'] = loc_price
+            json_data['prod_unit_price'] = loc_unit_price
+            json_data['prod_unit'] = loc_unit
+            json_data['category'] = loc_category
+            json_data['ins_dt'] = datetime.now().strftime("%Y%m%d")
+            full_data.append(json_data)
+        time.sleep(10)
+    logger.debug(missing_data)
+    logger.debug(full_data)
+    return full_data
+
+
+def getSkuList():
+    response = requests.get(occado_urls['base_url'])
     json_obj = json.loads(response.text)
     logger.info(len(json_obj['mainFopCollection']['sections'][0]['fops']))
-    full_data = []
+    sku_list = []
     for obj in json_obj['mainFopCollection']['sections'][0]['fops']:
-        json_data = {}
-        logger.info(obj)
-        loc_sku = obj['product']['sku']
-        loc_name = obj['product']['name']
-        loc_price = obj['product']['price']['current']
-        loc_unit_price = obj['product']['price']['unit']['price']
-        loc_unit = obj['product']['price']['unit']['per']
-        loc_category = obj['product']['mainCategory']
-        repl_chars = '£ ,!"^&*()%'
-        loc_offtext = obj['product']['offer']['text'].lower()
-        for char in repl_chars:
-            loc_offtext = loc_offtext.replace(char, '-')
-        loc_offtext = loc_offtext.replace('--', '-')
-        loc_offid = obj['product']['offer']['id']
-        logger.info(loc_sku)
-        json_data['prod_sku'] = loc_sku
-        json_data['prod_name'] = loc_name
-        json_data['prod_url'] = morris_urls['prod_base'].format(
-            loc_name.replace(' ', '-'), loc_sku)
-        json_data['promo_url'] = morris_urls['promo_base'].format(
-            loc_offtext, loc_offid)
-        json_data['prod_150_img'] = morris_urls['img_base'].format(
-            loc_sku, 150, 150, loc_sku[:3])
-        json_data['prod_640_img'] = morris_urls['img_base'].format(
-            loc_sku, 640, 640, loc_sku[:3])
-        json_data['prod_price'] = loc_price
-        json_data['prod_unit_price'] = loc_unit_price
-        json_data['prod_unit'] = loc_unit
-        json_data['category'] = loc_category
-        json_data['ins_dt'] = datetime.now().strftime("%Y%m%d")
-        full_data.append(json_data)
-        logger.info(full_data)
-        return full_data
+        sku_list.append(obj['sku'])
+    return sku_list
 
 
-if __name__ == "__main__":
-    offerProducts = getOffers()
+def genOutputFile(offerProducts):
     try:
         os.remove(FILE_NAME)
     except OSError:
         pass
     with io.open(FILE_NAME, 'w+', encoding='utf-8') as outfile:
         json.dump(offerProducts, outfile, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    skuList = getSkuList()
+    skuUrls = getSkuUrls(skuList)
+    #logger.info(skuUrls)
+    offerProducts = getOffers(skuUrls)
+    genOutputFile(offerProducts)
