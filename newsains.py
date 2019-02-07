f40d0811db9480b17b4e3087c1b2ff5a33c14190
ara <aravindc@sivapriya.org>
2019-02-07 17:23:09 +0000

    Sains without scrappy and with reroute

--- newsains.py
+++ newsains.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import requests
-import lxml.html
 import math
 import logging
 from datetime import datetime
 from messageq import openConnection
 from messageq import sendMessage
 from messageq import messageToFile
+from lxml import html
+from urllib.parse import urlsplit, parse_qs
 
 # Listed all product categories
 # 410380 - Special Offers
@@ -62,38 +64,85 @@ categories = [
     {"id": 12298, "name": "Pet"}
 ]
 
+def getCurrentIp():
+    r = requests.get('https://api.ipify.org?format=json', proxies=proxyDict)
+    logger.info('Using Ip: {0}'.format(r.text))
+
 def getSainsStartUrl():
         sains_start_url = []
-        # Added BWS category code to the list
-        # Dairy code changed from 267396 to Dairy, Eggs & Juice -  387873
-        # Added Special offers - 410380
-        # categoryId = [410380, 12518, 13343, 387873, 267397, 12320, 218831, 12422, 340854,
-        #              12192, 12448, 11651, 12564, 12298, 281806]
-        # categoryId=[12518]
-        # categoryId=[12518,13343,12422]
         urlstring = 'https://www.sainsburys.co.uk/shop/gb/groceries/home/' \
                     'CategorySeeAllView?langId=44&storeId=10151&catalogId=' \
                     '10123&pageSize=108&facet=88&categoryId=%d&' \
                     'categoryFacetId1=%d&beginIndex=%d'
         for n in categories:
-            print("Working on {0}".format(urlstring % (n['id'], n['id'], 0)))
+            logger.info("Working on {0}".format(urlstring % (n['id'], n['id'], 0)))
+            getCurrentIp()
             r = requests.get(urlstring %
                              (n['id'], n['id'], 0), proxies=proxyDict)
-            print(r)
-            data = lxml.html.fromstring(r.text)
+            logger.info(r)
+            data = html.fromstring(r.text)
             output = data.xpath('//h1[@class="resultsHeading"]/text()')
-            # item = output[0].replace('  ', '')
-            # .replace('\r\n', '').split('(')[0]
             itemcount = output[0].replace('  ', '') \
                                  .replace('\r\n', '') \
                                  .split('(')[1].split(' ')[0]
             for i in range(0, math.ceil(int(itemcount.replace(',', ''))/108)):
                 sains_start_url.append({"category":n['name'],"url":urlstring % (n['id'], n['id'], i*108)})
+                break
             break
         logger.info(sains_start_url)
         return sains_start_url
 
+def getProduct(offertag, category):
+    offer = {}
+    product_id_tag = './/div[@class="productInfo"]//div[@class=' \
+        '"promotion"]//a/@href'
+    offer_desc_tag = './/div[@class="productInfo"]//div[@class=' \
+        '"promotion"]//a/text()'
+    product_url = './/div[@class="productInfo"]//h3/a/@href'
+    product_desc_tag = './/div[@class="productInfo"]//h3/a/text()'
+    product_imgsrc_tag = './/div[@class="productInfo"]//h3/a/img/@src'
+    price_unit_tag = './/div[@class="pricing"]/p[@class="pricePerUnit"]' \
+        '/text()[1]'
+    offer['productid'] = parse_qs(urlsplit(offertag
+                                           .xpath(product_id_tag)[0])
+                                  .query)['productId'][0]
+    offer['imgsrcl'] = offertag.xpath(product_imgsrc_tag)[0]
+    offer['productdesc'] = offertag.xpath(product_desc_tag) \
+        [0].replace('  ', '') \
+        .replace('\r\n', '') \
+        .replace('Â£', '£')
+    offer['offerdesc'] = offertag.xpath(offer_desc_tag) \
+        [0].replace('  ', '') \
+        .replace('\r\n', '') \
+        .replace('Â£', '£')
+    offer['producturl'] = offertag.xpath(product_url)[0]
+    offer['priceunit'] = offertag.xpath(price_unit_tag)[0] \
+        .replace('  ', '') \
+        .replace('\r\n', '') \
+        .replace('\n', '') \
+        .replace('Â£','£')
+    offer['category'] = category
+    logger.info(offer)
+    return offer
+
+def getProducts(urls):
+    channel, connection = openConnection(exchangeName, queueName)
+    product_grid = '//div[@id="productsContainer"]/div[@id="product' \
+                       'Lister"]/ul[contains(@class,"productLister")]' \
+                       '//li[@class="gridItem"]'
+    for url in urls:
+        r = requests.get(url['url'], proxies=proxyDict)
+        tree = html.fromstring(r.content)
+        products = tree.xpath(product_grid)
+        for product in products:
+            sendMessage(exchangeName, queueName, getProduct(product,url['category']), channel)
+            logger.debug(getProduct(product, url['category']))
+            #break
+        #break
+    connection.close()
+    return None
 
 
 if __name__ == '__main__':
-    getSainsStartUrl()
+    urls = getSainsStartUrl()
+    items = getProducts(urls)
