1622ee661bb363fa70c9acb04f9b3b15a37da5f3
Aravidan Chockalingam <aravindc@sivapriya.org>
2021-03-22 10:00:11 +0000

    headers for tesc

--- newtesc.py
+++ newtesc.py
@@ -22,7 +22,18 @@ FILE_NAME = '/opt/offers/TESC_' + datetime.now().strftime("%Y%m%d") + '.json'
 exchangeName = 'TESC'
 queueName = '{0}_{1}'.format(exchangeName, datetime.now().strftime("%Y%m%d"))
 
-#c = Conxn()
+
+headers = {'User-Agent': 'PostmanRuntime/7.25.0',
+           'Accept': '*/*',
+           'Cache-Control': 'no-cache',
+           'Host': 'www.tesco.com',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Connection': 'keep-alive',
+           'Cookie': 'bm_sz=04919BE521C5C4D8ADF4617D5250A484~YAAQrpxkX+b8IYVyAQAA/VQr0QgTg5gDEXUmuUfa0qqtHv0QHHZjtL4gcSJ9RA7hoaEXJOTp1DYPb9xCrGwP37BrvtUY2kCKB7PqvVLXAXnfrt9F0ZiEPj10SiSVXZRZj8klW46ZA7Ho/0XtWlsO2aFX1MPkmD2/C10cDH6E1PgeO9EUNkZi9uPu109p4DE=; _abck=5621BD87FE69A39458BD0AB267BB9A81~-1~YAAQrpxkX+f8IYVyAQAA/VQr0QTSvxcBlxnRsND9THtPksH0EbfK/A3XkW0xT9oCk0Bj1ewbVDXr3PqtBjR7hHO6h6IXMvC2XID5RrAk0gVEKGwm9RDyBWyvp6hnPzicHMH6tTUIZdYLmssjIBAJ2WnpBkKUuF0YbX45V4H8d3m6u8FOhyqZewFyT1+Yvh14NDHwmDw4Yb4hQkLPglrkzt8LV39SpfSjjGkWMjyX4l967aCe+SHK5hjcTIz9bjSAoOQNqFWR5ATMnfBDSLOfaAQ4Dic=~-1~-1~-1; atrc=48693e75-78d9-4fce-85d0-9a0a50232644; _csrf=2wH2UKiamS-tjvd4hERekcG2',
+           'Referer': 'http://www.tesco.com/'}
+
+
+# c = Conxn()
 # Removed getTescoStartUrl and replaced with genCategoryUrlArray
 # This function provides starturl along with its category
 # def getTescoStartUrl():
@@ -59,7 +70,7 @@ def genCategoryUrlArray():
     base_url = 'https://www.tesco.com'
     tesc_cat_format_url = '/all?viewAll=promotion&promotion=offers&count=48&page={0}'
     grocery_html = requests.get(
-        url='https://www.tesco.com/groceries/en-GB/shop/', timeout=10)
+        url='https://www.tesco.com/groceries/en-GB/shop/', headers=headers, timeout=10)
     grocery_sub_nav_links = '//div[@class="current"]/ul[@class="list"]/li/a/@href'
     grocery_html_data = html.fromstring(grocery_html.text)
     category_links = grocery_html_data.xpath(grocery_sub_nav_links)
@@ -86,25 +97,26 @@ def genCategoryUrlArray():
                     transform_url + tesc_cat_format_url.format(i)
                 tesco_start_url.append(jsonObj)
                 break
-        except:
+        except Exception as e:
             pass
     return tesco_start_url
+
 
 def getProduct(Offertag, Category):
     offer = {}
     # XPath Tags
-    
-    #product_list_base_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div/'
-    #product_id_tag = './/div[@class="product-tile--wrapper"]//div[@class="tile-content"]/a/@href'
-    #product_imgsrc_tag = './/img/@src'
-    #product_desc_tag = './/div[@class="product-details--wrapper"]//a/text()'
-    #offer_desc_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div//div/span[@class = "offer-text"]/text()'
-    #validity_desc_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div//div/span[@class = "dates"]/text()'
+
+    # product_list_base_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div/'
+    # product_id_tag = './/div[@class="product-tile--wrapper"]//div[@class="tile-content"]/a/@href'
+    # product_imgsrc_tag = './/img/@src'
+    # product_desc_tag = './/div[@class="product-details--wrapper"]//a/text()'
+    # offer_desc_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div//div/span[@class = "offer-text"]/text()'
+    # validity_desc_tag = './/div[@class = "product-lists"]//ul[@class = "product-list grid"]/li[contains(@class, "product-list--list-item")]/div/div/div/div/div/div//div/span[@class = "dates"]/text()'
 
     product_list_base_tag = './div/div/div/div/'
     product_id_tag = product_list_base_tag + \
         '/div[@class="tile-content"]/a/@href'
-    product_imgsrc_tag = product_list_base_tag +'/img/@src'
+    product_imgsrc_tag = product_list_base_tag + '/img/@src'
     product_desc_tag = product_list_base_tag + '/h3/a/text()'
     offer_desc_tag = product_list_base_tag + \
         '/div/span[@class = "offer-text"]/text()'
@@ -139,26 +151,28 @@ def getProduct(Offertag, Category):
         offer['validitydesc'] = ""    
     return offer
 
+
 def getOffers(Urls):
     # channel, connection = openConnection(exchangeName, queueName)
     product_grid = '//div[@class="product-lists"]//ul[@class="product-list grid"]/li'
     for url in Urls:
-        #time.sleep(10)
-        r = requests.get(url['category_offer_url'], timeout=10)
+        # time.sleep(10)
+        r = requests.get(url['category_offer_url'], headers=headers, timeout=10)
         tree = html.fromstring(r.content)
         products = tree.xpath(product_grid)
         for product in products:
-            #sendMessage(exchangeName, queueName, getProduct(product,url['category']), channel)
+            # sendMessage(exchangeName, queueName, getProduct(product,url['category']), channel)
             logger.info(product)
-            #logger.info(getProduct(product, url['category']))
+            # logger.info(getProduct(product, url['category']))
             break
         break
-    #connection.close()
+    # connection.close()
     return None
+
 
 if __name__ == '__main__':
     urls = genCategoryUrlArray()
     getOffers(urls)
     channel, connection = openConnection(exchangeName, queueName)
-    #messageToFile(queueName, fileName=FILE_NAME)
+    # messageToFile(queueName, fileName=FILE_NAME)
     connection.close()    
